0e5411bf95f517830f08d7d36633f848faaa26cf
fxt <1375720973@qq.com>
2023-07-18 11:12:36 +0800

    y

--- TestSubmit.java
+++
@@ -1,5 +0,0 @@
-public class TestSubmit {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
--- hundreds/两数之和.java
+++ hundreds/两数之和.java
@@ -1,6 +1,8 @@
 package hundreds;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: jackie
@@ -8,6 +10,13 @@ import java.util.Arrays;
  **/
 public class 两数之和 {
 
+    /**
+     * 双指针法
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
@@ -21,6 +30,27 @@ public class 两数之和 {
             } else {
                 return new int[]{left, right};
             }
+        }
+        return new int[]{-1, -1};
+    }
+
+    /**
+     * map
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[]{-1, -1};
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
         }
         return new int[]{-1, -1};
     }
